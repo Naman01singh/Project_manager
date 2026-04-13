ec716b5515cdc880667015fbe6e1a0c2980882d7
Naman01singh <naman157568@gmail.com>
2026-04-14 00:09:53 +0530

    added

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -9,5 +9,5 @@ router.route("/register").post(userRegisterValidator(), validate, registerUser);
 router.route("/login").post(userLoginValidator(), validate, login);
 
 //secure routes
-router.route("/logout").post(verifyJWT,);
+router.route("/logout").post(verifyJWT,logoutUser);
 export default router;
